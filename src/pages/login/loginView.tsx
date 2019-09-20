dfc80a60fd4687fc2b00401b5bf1b51daebd22e2
Matías Varela <mvarela@tsoftglobal.com>
2019-09-20 15:32:04 -0300

    cambio msj password

--- src/pages/login/loginView.tsx
+++ src/pages/login/loginView.tsx
@@ -50,7 +50,7 @@ class LoginView extends React.Component<any, LoginViewState> {
                             performLogin={this.loginUser}
                             userAuthorized={this.redirectLogin}
                             userLabel="Usuario"
-                            passwordLabel="Pass " />
+                            passwordLabel="Contraseña" />
                         {/* </div> */}
                     </Grid.Row>
                     <Grid.Row>
